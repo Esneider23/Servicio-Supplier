25e1d01d0f1fefb746bba573ddfeb9e11e6b77e6
Esneider23 <guzmane@utb.edu.co>
2022-10-15 15:42:10 -0500

    server

--- app.py
+++ app.py
@@ -1,43 +1,42 @@
 # we invoke the necessary libraries
-from flask import Flask, render_template, request, jsonify
-from config import config
+from flask import Flask, render_template, jsonify
 from flask_mysqldb import MySQL
 import contraller
 
 # The access point is created
-app = Flask(__name__)
+server = Flask(__name__)
 
 # The connection point to the base is created
 server.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
 server.config['MYSQL_USER'] = 'bee0e9755133d2'
 server.config['MYSQL_PASSWORD'] = 'f3e9360a'
 server.config['MYSQL_DB'] = 'heroku_23edc9681868d22'
-mysql = MySQL(app)
+mysql = MySQL(server)
 
 
 # The route to enter the service is created
-@app.get('/supplier')
+@server.get('/supplier')
 def index():
     try:
-        return contraller.showSupplier(mysql)
-    except Exception as ex:
-        return page_not_found(ex)
+        return contraller.show_supplier(mysql)
+    except Exception as error:
+        return jsonify({"Message": error})
 
 
 # this route is created to remove data from this service
-@app.delete('/supplier/<id>')
-def supplierDelete(id):
+@server.delete('/supplier/<id>')
+def supplier_delete(id):
     try:
-        return contraller.deleteSupplier(mysql, id)
+        return contraller.delete_supplier(mysql, id)
     except Exception as ex:
         return page_not_found(ex)
 
 
 # this route is created to update data of this service
-@app.put('/supplier/<id>')
-def supplierUpdate(id):
+@server.put('/supplier/<id>')
+def supplier_update(id):
     try:
-        return contraller.updateSupplier(mysql, id)
+        return contraller.update_supplier(mysql, id)
     except Exception as ex:
         return page_not_found(ex)
 
@@ -49,4 +48,4 @@ def page_not_found(error):
 
 # the application is executed
 if __name__ == '__main__':
-    app.run(debug=True)
+    server.run(debug=True)
--- contraller.py
+++ contraller.py
@@ -3,7 +3,7 @@ from flask import jsonify, request
 
 
 # This function returns the supplier information
-def showSupplier(mysql):
+def show_supplier(mysql):
     try:
         cursor = mysql.connection.cursor()
         cursor.execute("select idsupplier, name, nit, address, phone, email from supplier")
@@ -13,35 +13,37 @@ def showSupplier(mysql):
             for fila in data:
                 listSupplierGet = {'idsupplier': fila[0], 'name': fila[1], 'nit': fila[2], 'address': fila[3], 'phone': fila[4], 'email': fila[5]}
                 listSupplier.append(listSupplierGet)
-            print(data)
             return jsonify({'listSupplier': listSupplier, 'messeger': "Cursos listado"})
         else:
-            return jsonify({'messeger: "There are no data'})
+            return jsonify({'message: "There are no data'})
     except Exception as ex:
-        return jsonify({'messeger: "Error'})
+        return jsonify({'message: "Error'})
 
 
 # This function deletes a data from a supplier
-def deleteSupplier(mysql, idSupplier):
-    cursor = mysql.connection.cursor()
-    cursor.execute("delete from supplier where idsupplier = (%s)", (idSupplier,))
-    data = cursor.fetchall()
-    print("Provider to be deleted: ", (data))
-    cursor.execute("select idsupplier, name, nit, address, phone, email from supplier where idsupplier = (%s)",
-                 (idSupplier,))
-    mysql.connection.commit()
-
-    return showSupplier(mysql)
+def delete_supplier(mysql, idSupplier):
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("delete from supplier where idsupplier = (%s)", (idSupplier,))
+        data = cursor.fetchall()
+        print("Provider to be deleted: ", (data))
+        cursor.execute("select idsupplier, name, nit, address, phone, email from supplier where idsupplier = (%s)",
+                     (idSupplier,))
+        mysql.connection.commit()
+        return show_supplier(mysql)
+    except Exception as ex:
+        return jsonify({'message: "Error'})
 
 
 # This function updates a data of a supplier
 def updateSupplier(mysql, idSupplier):
-    cursor = mysql.connection.cursor()
-    cursor.execute("update supplier set name = '{0}', nit = '{1}', address = '{2}', phone = '{3}',"
-                   "email = '{4}' where idsupplier = '{5}'".format(request.json['name'], request.json['nit'],
-                                                                   request.json['address'], request.json['phone'],
-                                                                   request.json['email'], idSupplier))
-    mysql.connection.commit()
-    return showSupplier(mysql)
-
-
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("update supplier set name = '{0}', nit = '{1}', address = '{2}', phone = '{3}',"
+                       "email = '{4}' where idsupplier = '{5}'".format(request.json['name'], request.json['nit'],
+                                                                       request.json['address'], request.json['phone'],
+                                                                       request.json['email'], idSupplier))
+        mysql.connection.commit()
+        return show_supplier(mysql)
+    except Exception as ex:
+        return jsonify({'message: "Error'})
