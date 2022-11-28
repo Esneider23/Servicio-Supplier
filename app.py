9dfa864c7fd73e197b909eaae77fb80e1a507618
KemapaX <kpalmieri@utb.edu.co>
2022-11-28 18:00:14 -0500

    Agregando CORS of Servicios-Supplier

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # we invoke the necessary libraries
 from flask import Flask, render_template, jsonify
 from flask_mysqldb import MySQL
+from flask_cors import CORS, cross_origin
 import contraller
 
 # The access point is created
 server = Flask(__name__)
+
+CORS(server)
 
 # The connection point to the base is created
 server.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
@@ -14,6 +17,7 @@ server.config['MYSQL_DB'] = 'heroku_23edc9681868d22'
 mysql = MySQL(server)
 
 
+@cross_origin
 # The route to enter the service is created
 @server.get('/supplier')
 def index():
@@ -23,6 +27,7 @@ def index():
         return jsonify({"Message": error})
 
 
+@cross_origin
 # this route is created to remove data from this service
 @server.delete('/supplier/<id>')
 def supplier_delete(id):
@@ -32,6 +37,7 @@ def supplier_delete(id):
         return page_not_found(ex)
 
 
+@cross_origin
 # this route is created to update data of this service
 @server.put('/supplier/<id>')
 def supplier_update(id):
@@ -41,6 +47,7 @@ def supplier_update(id):
         return page_not_found(ex)
 
 
+@cross_origin
 # A function is created to show when a page is not found.
 def page_not_found(error):
     return render_template('404.html')
